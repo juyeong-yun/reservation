a6c03d512b6b4defc03000e11cfbcc982e259b5d
juyeong <yp9782@gmail.com>
2024-06-14 13:03:46 +0900

    240614 / thymeleaf 연결

--- src/main/java/seon/gallery/reservation/controller/GuestController.java
+++ src/main/java/seon/gallery/reservation/controller/GuestController.java
@@ -9,14 +9,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-@RequestMapping("/guest")
+@RequestMapping("/reservation")
 public class GuestController {
 
+	/**
+	 * board 로 가는
+	 * @return
+	 */
     @GetMapping("/board")
     public String board() {
     	
-        return "guest/board";
+        return "/guest/board";
     }
     
+    /**
+     * 전시소개 페이지 
+     * @return
+     */
+    @GetMapping("/about")
+    public String about() {
+    	
+        return "/guest/about";
+    }
+    
+    /**
+     * 예약 페이지
+     * @return
+     */
+    @GetMapping("/reservation")
+    public String reserve() {
+    	
+        return "/guest/reservation";
+    }
+    
+    @GetMapping("/reviews")
+    public String reviews() {
+    	
+        return "/write/reviews";
+    }
     
 }
