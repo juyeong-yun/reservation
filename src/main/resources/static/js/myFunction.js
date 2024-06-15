d256ad9186cbaec97aa86fdf86ee5d5675639277
juyeong <yp9782@gmail.com>
2024-06-15 15:12:09 +0900

    240615 / 예약 시간을 누르면 모달창이 뜨게 해놓았음

--- src/main/resources/static/js/myFunction.js
+++ src/main/resources/static/js/myFunction.js
@@ -10,10 +10,37 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+// ckeditor 불러오는 api
 document.addEventListener("DOMContentLoaded", function() {
 	ClassicEditor
 		.create(document.querySelector('#editor'))
 		.catch(error => {
                     console.error(error);
         });
+});
+
+// modal 창과 연결된 js
+$(function() {
+    // Variable to store the href to navigate to upon confirmation
+    let targetHref;
+
+    // When modal-open link is clicked
+    $(document).on("click", "#modal-open", function(e) {
+		
+        e.preventDefault(); // Prevent the default action
+        targetHref = $(this).data("href"); // Store the href for later
+        $("#popup").css('display', 'flex').hide().fadeIn(); // Show the modal
+    });
+
+    // When the close button is clicked
+    $("#close").click(function() {
+        modalClose(); // Call the modal close function
+        // window.location.href = targetHref; // Redirect to the stored href
+        // 이거에 대한 가는 게 필요한 거구나
+    });
+
+    // Modal close function
+    function modalClose() {
+        $("#popup").fadeOut(); // Fade out effect for closing modal
+    }
 });
