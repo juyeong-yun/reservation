84b19ebd585f3e36fc11f74d4657ab594793bbd7
juyeong <yp9782@gmail.com>
2024-06-18 17:41:49 +0900

    240618 / review 데이터베이스 연결

---
+++ src/main/java/seon/gallery/reservation/dto/ReviewDTO.java
@@ -0,0 +1,41 @@
+package seon.gallery.reservation.dto;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import seon.gallery.reservation.entity.ReviewEntity;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+public class ReviewDTO {
+
+	private Long reviewId;
+	private String reviewName;
+	private String reviewTitle;
+	private String reviewDetail;
+	private LocalDateTime writeDate;
+	private LocalDateTime updateDate;
+	private MultipartFile reviewImage;
+	
+	public static ReviewDTO toDTO(ReviewEntity entity) {
+		return ReviewDTO.builder()
+				.reviewId(entity.getReviewId())
+				.reviewName(entity.getReviewName())
+				.reviewTitle(entity.getReviewTitle())
+				.reviewDetail(entity.getReviewDetail())
+				.writeDate(entity.getWriteDate())
+				.updateDate(entity.getUpdateDate())
+				.build();
+	}
+}
---
+++ src/main/java/seon/gallery/reservation/entity/ReviewEntity.java
@@ -0,0 +1,59 @@
+package seon.gallery.reservation.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import seon.gallery.reservation.dto.ReviewDTO;
+import seon.gallery.reservation.dto.check.YesorNo;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+@Entity
+@Table(name="review")
+public class ReviewEntity {
+	@Id
+	@Column(name="review_num")
+	private Long reviewId;
+	
+	@Column(name="review_name")
+	private String reviewName;
+	
+	@Column(name="review_title")
+	private String reviewTitle;
+	
+	@Column(name="review_detail")
+	private String reviewDetail;
+	
+	@Column(name="write_date")
+	private LocalDateTime writeDate;
+	
+	@Column(name="update_date")
+	private LocalDateTime updateDate;
+	
+	public static ReviewEntity toEntity(ReviewDTO dto) {
+		return ReviewEntity.builder()
+				.reviewId(dto.getReviewId())
+				.reviewName(dto.getReviewName())
+				.reviewTitle(dto.getReviewTitle())
+				.reviewDetail(dto.getReviewDetail())
+				.writeDate(dto.getWriteDate())
+				.updateDate(dto.getUpdateDate())
+				.build();
+	}
+
+}
