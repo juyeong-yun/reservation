f344af7733ba60378a65245f03a21c592bad0908
juyeong <yp9782@gmail.com>
2025-05-21 15:49:28 +0900

    250521 / merge

--- src/main/java/seon/gallery/reservation/controller/MainController.java
+++ src/main/java/seon/gallery/reservation/controller/MainController.java
@@ -2,10 +2,7 @@ package seon.gallery.reservation.controller;
 
 import org.springframework.stereotype.Controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class MainController {
